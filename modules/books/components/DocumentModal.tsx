1a70d69276d550e097422191256267519f083478
Lorena-Cec <ceclorenalc@gmail.com>
2024-10-29 11:44:47 +0100

    add books modal - pdf working, epub design fix

--- modules/books/components/DocumentModal.tsx
+++ modules/books/components/DocumentModal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { FaTimes } from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaTimes } from "react-icons/fa";
 import ePub, { Rendition, Book } from "epubjs";
 
 interface DocumentModalProps {
@@ -15,30 +15,20 @@ const DocumentModal: React.FC<DocumentModalProps> = ({
 }) => {
   const viewerRef = useRef<HTMLDivElement>(null);
   const isEpub = documentUrl.includes(".epub");
+  const isPdf = documentUrl.includes(".pdf");
   const [error, setError] = useState<string | null>(null);
   const [rendition, setRendition] = useState<Rendition | null>(null);
-  const [book, setBook] = useState<Book | null>(null);
-  const [currentSectionIndex, setCurrentSectionIndex] = useState<number>(0);
-  const [totalSections, setTotalSections] = useState<number>(0);
+  const [, setBook] = useState<Book | null>(null);
 
-  // Navigacija između sekcija
-  const goToNextSection = async () => {
-    if (rendition && currentSectionIndex < totalSections - 1) {
-      const nextSection = book?.spine.get(currentSectionIndex + 1);
-      if (nextSection?.href) {
-        await rendition.display(nextSection.href);
-        setCurrentSectionIndex(currentSectionIndex + 1);
-      }
+  const goToNextPage = async () => {
+    if (rendition) {
+      await rendition.next();
     }
   };
 
-  const goToPreviousSection = async () => {
-    if (rendition && currentSectionIndex > 0) {
-      const previousSection = book?.spine.get(currentSectionIndex - 1);
-      if (previousSection?.href) {
-        await rendition.display(previousSection.href);
-        setCurrentSectionIndex(currentSectionIndex - 1);
-      }
+  const goToPreviousPage = async () => {
+    if (rendition) {
+      await rendition.prev();
     }
   };
 
@@ -58,28 +48,27 @@ const DocumentModal: React.FC<DocumentModalProps> = ({
 
           await newBook.loaded.navigation;
 
-          // Proverite spine i broj sekcija
           console.log("Spine content:", newBook.spine);
 
-          // Prebrojte sekcije
           let sectionCount = 0;
           newBook.spine.each(() => sectionCount++);
-          setTotalSections(sectionCount);
           console.log("Total sections:", sectionCount);
 
-          // Uništi prethodni prikaz ako postoji
           if (rendition) rendition.destroy();
 
           const newRendition = newBook.renderTo(viewerRef.current, {
             width: "100%",
             height: "100%",
-            flow: "scrolled",
+            flow: "paginated",
+            spread: "auto",
           });
           setRendition(newRendition);
-          const firstSection = newBook.spine.get(0); // Prva prava sekcija
+          const firstSection = newBook.spine.get(0);
           if (firstSection?.href) {
             await newRendition.display(firstSection.href);
           }
+        } else if (isPdf) {
+          setError(null);
         } else {
           throw new Error("Invalid document or viewer reference");
         }
@@ -100,45 +89,54 @@ const DocumentModal: React.FC<DocumentModalProps> = ({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="relative bg-white rounded-lg shadow-lg w-2/3 h-full p-4 text-brown-100">
+      <div className="relative bg-white rounded-lg shadow-lg w-2/3 h-0.9 p-4 text-brown-100">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-semibold">{title}</h2>
           <button onClick={onClose}>
             <FaTimes className="text-2xl" />
           </button>
         </div>
-        <div className="h-5/6 overflow-auto">
+        <div
+          className={`${isEpub ? "h-5/6" : "h-full"} overflow-hidden relative`}
+        >
+          {" "}
           {error ? (
             <p className="text-red-500">{error}</p>
           ) : isEpub ? (
-            <div ref={viewerRef} style={{ height: "100%" }} className="view" />
-          ) : (
+            <div
+              ref={viewerRef}
+              style={{
+                height: "100%",
+              }}
+              className="view"
+            />
+          ) : isPdf ? (
             <iframe
               src={documentUrl}
               title="Document Viewer"
-              className="w-full h-full"
-              sandbox="allow-same-origin allow-popups allow-forms"
+              className="w-full"
+              style={{
+                height: "94%",
+              }}
             />
+          ) : null}
+          {isEpub && (
+            <>
+              <button
+                onClick={goToPreviousPage}
+                className="absolute left-0 top-1/2 transform -translate-y-1/2 pr-2"
+              >
+                <FaChevronLeft className="text-2xl" />
+              </button>
+              <button
+                onClick={goToNextPage}
+                className="absolute right-0 top-1/2 transform -translate-y-1/2 pl-2"
+              >
+                <FaChevronRight className="text-2xl" />
+              </button>
+            </>
           )}
         </div>
-        {isEpub && (
-          <div className="flex justify-between mt-4">
-            <button
-              onClick={goToPreviousSection}
-              disabled={currentSectionIndex === 0}
-              className="px-4 py-2 bg-gray-200 rounded"
-            >
-              Previous Section
-            </button>
-            <button
-              onClick={goToNextSection}
-              disabled={currentSectionIndex >= totalSections - 1}
-              className="px-4 py-2 bg-gray-200 rounded"
-            >
-              Next Section
-            </button>
-          </div>
-        )}
       </div>
     </div>
   );
